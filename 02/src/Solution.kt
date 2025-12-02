929d5bc5a2af16a9d6b121ab2c20e04824f08f83
Macon Pegram <mpegram@captechconsulting.com>
2025-12-02 15:07:17 -0500

    Part 2 complete

--- 02/src/Solution.kt
+++ 02/src/Solution.kt
@@ -5,15 +5,18 @@ fun main() {
     val garbIn = File("puzzle-input.txt")
     val rangeStr = garbIn.readText(Charset.defaultCharset())
     val ranges = extractRanges(rangeStr)
+
     val solutionOne = calculateSolutionOne(ranges)
     println("Solution One: $solutionOne")
 
+    val solutionTwo = calculateSolutionTwo(ranges)
+    println("Solution Two: $solutionTwo")
 }
 
 // Input parser
 fun extractRanges(rangeStr: String): List<LongRange> {
-    return rangeStr.split(",").map {
-        val bounds = it.split("-").map { it.toLong() }
+    return rangeStr.split(",").map { rangeStr ->
+        val bounds = rangeStr.split("-").map { rangeStr.toLong() }
         bounds[0]..bounds[1]
     }
 }
@@ -39,3 +42,48 @@ fun calculateSolutionOne(ranges: List<LongRange>): Long {
 
     return tally
 }
+
+// Part 2, look for numbers that have any number of repeating sequences
+fun calculateSolutionTwo(ranges: List<LongRange>): Long {
+    var tally: Long = 0
+
+    // Another case of brute forcing it (for now).
+    for (range in ranges) {
+        for (num in range) {
+            val numStr = num.toString()
+
+            // We only need to check up to half the length of the string
+            // Anything longer can't be a repeating pattern
+            val maxIndex = (numStr.length / 2)
+            for (i in 1..maxIndex) {
+                // Snag the prefix
+                val left = numStr.take(i)
+
+                // Now get the rest of the string which we'll chip away at
+                // by seeing if it starts with the prefix
+                var right = numStr.substring(i)
+
+                // Iterate over the right side, matching the left side in chunks
+                // If we can fully consume the right side, we have a match
+                while (right.isNotEmpty()) {
+
+                    // Check if the left side is a prefix of the remaining string
+                    if (left.length <= right.length && right.startsWith(left)) {
+                        // Chop off the prefix leaving the rest of the string.
+                        right = right.substring(left.length)
+                    } else {
+                        // If the prefix can't be matched no point continuing to look for pattern
+                        break
+                    }
+                }
+                // If we make it here we have a repeating pattern so add it to the tally
+                if (right.isEmpty()) {
+                    tally += num
+                    break
+                }
+            }
+        }
+    }
+    return tally
+}
+
