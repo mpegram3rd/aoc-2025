21cab0f025f315f7c7e237a5ed6313c6b0d2c4fc
Macon Pegram <mpegram@captechconsulting.com>
2025-12-02 15:20:03 -0500

    Refactored to do a single pass through each of the numbers in range for both solutions

--- 02/src/Solution.kt
+++ 02/src/Solution.kt
@@ -5,85 +5,82 @@ fun main() {
     val garbIn = File("puzzle-input.txt")
     val rangeStr = garbIn.readText(Charset.defaultCharset())
     val ranges = extractRanges(rangeStr)
-
-    val solutionOne = calculateSolutionOne(ranges)
-    println("Solution One: $solutionOne")
-
-    val solutionTwo = calculateSolutionTwo(ranges)
-    println("Solution Two: $solutionTwo")
+    processRanges(ranges)
 }
 
 // Input parser
 fun extractRanges(rangeStr: String): List<LongRange> {
     return rangeStr.split(",").map { rangeStr ->
-        val bounds = rangeStr.split("-").map { rangeStr.toLong() }
+        val bounds = rangeStr.split("-").map { it.toLong() }
         bounds[0]..bounds[1]
     }
 }
 
-// Find all numbers in the ranges that have exactly 2 repeating sequences
-fun calculateSolutionOne(ranges: List<LongRange>): Long {
-    var tally: Long = 0
+// Refactored to make processing the ranges for both solutions a single pass
+fun processRanges(ranges: List<LongRange>) {
+    var sol1Tally: Long = 0
+    var sol2Tally: Long = 0
 
-    // gonna just brute force this initially though I think a more clever
-    // approach might be working with powers of 10.
     for (range in ranges) {
         for (num in range) {
             val numStr = num.toString()
-            // Only worry about values that are even length so
-            if (numStr.length % 2 != 0) {
-                continue
-            }
-            val left = numStr.take(numStr.length / 2)
-            val right = numStr.substring(numStr.length / 2)
-            tally += if (left == right) num else 0
+            sol1Tally += if (calculateSolutionOne(numStr)) num else 0
+            sol2Tally += if (calculateSolutionTwo(numStr)) num else 0
         }
     }
+    println("Solution One: $sol1Tally")
+    println("Solution Two: $sol2Tally")
+}
 
-    return tally
+// Find all numbers in the ranges that have exactly 2 repeating sequences
+fun calculateSolutionOne(numStr: String): Boolean {
+    // Only worry about values that are even length so
+    if (numStr.length % 2 != 0) {
+        return false
+    }
+
+    // Split the string in half and compare
+    val left = numStr.take(numStr.length / 2)
+    val right = numStr.substring(numStr.length / 2)
+
+    // check if both halves match
+    return left == right
 }
 
 // Part 2, look for numbers that have any number of repeating sequences
-fun calculateSolutionTwo(ranges: List<LongRange>): Long {
-    var tally: Long = 0
+// This is a bit more brute force than I'd like but has some minor optimizations
+fun calculateSolutionTwo(numStr: String): Boolean {
 
-    // Another case of brute forcing it (for now).
-    for (range in ranges) {
-        for (num in range) {
-            val numStr = num.toString()
+    // We only need to check up to half the length of the string
+    // Anything longer can't be a repeating pattern
+    val maxIndex = (numStr.length / 2)
+    for (i in 1..maxIndex) {
 
-            // We only need to check up to half the length of the string
-            // Anything longer can't be a repeating pattern
-            val maxIndex = (numStr.length / 2)
-            for (i in 1..maxIndex) {
-                // Snag the prefix
-                val left = numStr.take(i)
+        // Snag the prefix
+        val left = numStr.take(i)
 
-                // Now get the rest of the string which we'll chip away at
-                // by seeing if it starts with the prefix
-                var right = numStr.substring(i)
+        // Now get the rest of the string which we'll chip away at
+        // by seeing if it starts with the prefix
+        var right = numStr.substring(i)
 
-                // Iterate over the right side, matching the left side in chunks
-                // If we can fully consume the right side, we have a match
-                while (right.isNotEmpty()) {
+        // Iterate over the right side, matching the left side in chunks
+        // If we can fully consume the right side, we have a match
+        while (right.isNotEmpty()) {
 
-                    // Check if the left side is a prefix of the remaining string
-                    if (left.length <= right.length && right.startsWith(left)) {
-                        // Chop off the prefix leaving the rest of the string.
-                        right = right.substring(left.length)
-                    } else {
-                        // If the prefix can't be matched no point continuing to look for pattern
-                        break
-                    }
-                }
-                // If we make it here we have a repeating pattern so add it to the tally
-                if (right.isEmpty()) {
-                    tally += num
-                    break
-                }
+            // Check if the left side is a prefix of the remaining string
+            if (left.length <= right.length && right.startsWith(left)) {
+                // Chop off the prefix leaving the rest of the string.
+                right = right.substring(left.length)
+            } else {
+                // If the prefix can't be matched no point continuing to look for pattern
+               break
             }
         }
+        // If we make it here we have a repeating pattern so add it to the tally
+        if (right.isEmpty()) {
+            return true
+        }
     }
-    return tally
+    return false
 }
 
