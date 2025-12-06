6bf8fad12166712ad892b81c2bb361997f048a49
mpegram3rd <mpergam3rd@hotmail.com>
2025-12-06 10:28:44 -0500

    Day 6 template setup

---
+++ 06/src/Solution.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main() {
+
+    val garbIn = File("puzzle-input.txt")
+
+    println("Solution One: ${solutionOne()}")
+    println("Solution Two: ${solutionTwo()}")
+
+}
+
+fun solutionOne(): Long {
+    return 0
+}
+
+fun solutionTwo(): Long {
+    return 0
+}
+
